ca9e5ae951279e6c1c4b6afa77ebf22cdaf55451
Indrek Luuk <indrek.luuk@gmail.com>
2016-10-30 22:12:14 +0200

    example tests are now running properly

---
+++ test/src/exampletest.cpp
@@ -0,0 +1,18 @@
+//
+// Created by indrek on 30.10.2016.
+//
+
+
+#include "gtest/gtest.h"
+
+
+
+
+
+TEST(ExampleTest, testSomething) {
+  ASSERT_EQ(1, 1);
+}
+
+
+
+
---
+++ test/src/exampletest2.cpp
@@ -0,0 +1,18 @@
+//
+// Created by indrek on 30.10.2016.
+//
+
+
+#include "gtest/gtest.h"
+
+
+
+
+
+TEST(ExampleTest2, testSomething2) {
+  ASSERT_EQ(1, 0);
+}
+
+
+
+
