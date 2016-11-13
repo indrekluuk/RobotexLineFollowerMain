637ee57f192a0d92a0d2ca843a908b0a98ac38bf
Indrek Luuk <indrek.luuk@gmail.com>
2016-11-13 16:52:25 +0200

    test turn other way

--- test/src/line/LineTest.cpp
+++ test/src/line/LineTest.cpp
@@ -39,7 +39,7 @@ TEST(LineTest, testLineFound) {
 
 TEST(LineTest, testLineAcrossTheScreen) {
   Line line;
-  for (int i=0; i<Line::getRowCount(); i++) {
+  for (uint8_t i=0; i<Line::getRowCount(); i++) {
     line.setRowBitmap(i, 0b000010000, 0b000000000);
   }
   ASSERT_TRUE(line.isLineFound());
@@ -77,10 +77,33 @@ TEST(LineTest, testLineTurnsInSlantDirection) {
   line.setRowBitmap(6,  0b00000000, 0b11110000);
   line.setRowBitmap(7,  0b00000111, 0b10000000);
   line.setRowBitmap(8,  0b00111100, 0b00000000);
-  line.setRowBitmap(9,  0b11100000, 0b00000000);
+  line.setRowBitmap(9,  0b01100000, 0b00000000);
   line.setRowBitmap(10, 0b00000000, 0b00000000);
   ASSERT_TRUE(line.isLineFound());
   ASSERT_EQ(-9, line.getLinePosition());
+}
+
+
+
+
+TEST(LineTest, testLineTurnsAwayFromSlantDirection) {
+  Line line;
+  line.setRowBitmap(0,  0b00000010, 0b00000000);
+  line.setRowBitmap(1,  0b00000010, 0b00000000);
+  line.setRowBitmap(2,  0b00000100, 0b00000000);
+  line.setRowBitmap(3,  0b00000100, 0b00000000);
+  line.setRowBitmap(4,  0b00001100, 0b00000000);
+  line.setRowBitmap(5,  0b00001000, 0b00000000);
+  line.setRowBitmap(6,  0b00001000, 0b00000000);
+  line.setRowBitmap(7,  0b00011000, 0b00000000);
+  line.setRowBitmap(8,  0b00100000, 0b00000000);
+  line.setRowBitmap(9,  0b00111111, 0b10000000);
+  line.setRowBitmap(10, 0b00000000, 0b11111110);
+  line.setRowBitmap(10, 0b00000000, 0b00000000);
+  line.setRowBitmap(10, 0b00000000, 0b00000000);
+  line.setRowBitmap(10, 0b00000000, 0b00000000);
+  ASSERT_TRUE(line.isLineFound());
+  ASSERT_EQ(11, line.getLinePosition());
 }
 
 
