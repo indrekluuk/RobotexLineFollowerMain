1ae17f1bffe8882aa652eac18d3bddb3eece991d
Indrek Luuk <indrek.luuk@gmail.com>
2016-11-13 16:56:11 +0200

    additional tests

--- test/src/line/LineTest.cpp
+++ test/src/line/LineTest.cpp
@@ -99,12 +99,54 @@ TEST(LineTest, testLineTurnsAwayFromSlantDirection) {
   line.setRowBitmap(8,  0b00100000, 0b00000000);
   line.setRowBitmap(9,  0b00111111, 0b10000000);
   line.setRowBitmap(10, 0b00000000, 0b11111110);
-  line.setRowBitmap(10, 0b00000000, 0b00000000);
-  line.setRowBitmap(10, 0b00000000, 0b00000000);
-  line.setRowBitmap(10, 0b00000000, 0b00000000);
+  line.setRowBitmap(11, 0b00000000, 0b00000000);
+  line.setRowBitmap(12, 0b00000000, 0b00000000);
+  line.setRowBitmap(13, 0b00000000, 0b00000000);
   ASSERT_TRUE(line.isLineFound());
   ASSERT_EQ(11, line.getLinePosition());
 }
 
+
+
+
+TEST(LineTest, testIgnoreOtherLine) {
+  Line line;
+  line.setRowBitmap(0,  0b00000010, 0b00000000);
+  line.setRowBitmap(1,  0b00000010, 0b00000000);
+  line.setRowBitmap(2,  0b00000010, 0b00000000);
+  line.setRowBitmap(3,  0b00000100, 0b00100000);
+  line.setRowBitmap(4,  0b00000100, 0b00100000);
+  line.setRowBitmap(5,  0b00001000, 0b00110000);
+  line.setRowBitmap(6,  0b00000000, 0b00010000);
+  line.setRowBitmap(7,  0b00000000, 0b00011000);
+  line.setRowBitmap(8,  0b00000000, 0b00001000);
+  line.setRowBitmap(9,  0b00000000, 0b00001000);
+  line.setRowBitmap(10, 0b00000000, 0b00000000);
+
+  ASSERT_TRUE(line.isLineFound());
+  ASSERT_EQ(7, line.getLinePosition());
+}
+
+
+
+
+
+TEST(LineTest, testIgnoreLineAfter) {
+  Line line;
+  line.setRowBitmap(0,  0b00000010, 0b00000000);
+  line.setRowBitmap(1,  0b00000010, 0b00000000);
+  line.setRowBitmap(2,  0b00000010, 0b00000000);
+  line.setRowBitmap(3,  0b00000100, 0b00000000);
+  line.setRowBitmap(4,  0b00000100, 0b00000000);
+  line.setRowBitmap(5,  0b00001000, 0b00000000);
+  line.setRowBitmap(6,  0b00000000, 0b00000000);
+  line.setRowBitmap(7,  0b00000000, 0b00001000);
+  line.setRowBitmap(8,  0b00000000, 0b00001000);
+  line.setRowBitmap(9,  0b00000000, 0b00001000);
+  line.setRowBitmap(10, 0b00000000, 0b00000000);
+
+  ASSERT_TRUE(line.isLineFound());
+  ASSERT_EQ(7, line.getLinePosition());
+}
 
 
